09cc61f74e38c69cd535df68371f24684da2783f
davidcoats <david@getsafetycone.com>
2024-02-25 12:33:47 -0800

    Initial commit.

---
+++ source/D8S.E0014/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace D8S.E0014
+{
+	/// <summary>
+	/// JSON file generation using System.Text.Json.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/D8S.E0014/Code/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+
+namespace D8S.E0014
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
